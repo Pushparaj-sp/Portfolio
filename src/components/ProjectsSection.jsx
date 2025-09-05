b53a0ffde6ba7c18b6ce43eba22bf3a09e475d9f
Pushparaj-sp <spushparaj8807@gmail.com>
2025-09-05 19:44:17 +0530

    update css style and config

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -5,7 +5,7 @@ const projects = [
     id: 1,
     title: "GlowGroove - E-Commerce Website",
     description: "A beautiful landing page app using FrontEnd-development.",
-    image: "./certificates/glow.png", // ✅ updated
+    image: "./glow.png", // ✅ updated
     tags: ["HTML", "TailwindCSS", "Javascript"],
     demoUrl: "https://pushparaj-sp-23.neocities.org/GlowGroove/",
     githubUrl: "https://pushparaj-sp-23.neocities.org/GlowGroove/",
@@ -15,7 +15,7 @@ const projects = [
     title: "VIT Website- Cloning ",
     description:
       "A VIT Admission website clone providing details, forms, and resources for students apply .",
-    image: "./certificates/vite.png",
+    image: "./vitee.png",
     tags: ["ReactJS", "HTML", "CSS"],
     demoUrl: "https://github.com/Pushparaj-sp/VIT",
     githubUrl: "https://github.com/Pushparaj-sp/VIT",
@@ -25,7 +25,7 @@ const projects = [
     title: "Memocare - A Mobile App",
     description:
       "Smart Memory and Safety Assistant for Amnesia Patients.",
-    image: "./certificates/amnesia.jpg",
+    image: "./amnesia.jpg",
     tags: ["React", "Dart","Flutter"],
     demoUrl: "https://github.com/Pushparaj-sp/memocare/",
     githubUrl: "https://github.com/Pushparaj-sp/memocare/",
