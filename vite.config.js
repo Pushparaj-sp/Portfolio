d076a4a5cad2bbce82ebc6fa27d9beb89b81c52d
Pushparaj-sp <spushparaj8807@gmail.com>
2025-09-05 15:02:49 +0530

    update css and config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  base: '/Portfolio/', // for GitHub Pages
+  base: '/Portfolio', // for GitHub Pages
   plugins: [react()],
   resolve: {
     alias: {
