a5a25cfe5912416bf451e7b82e51dc5dbc8d5c88
Pushparaj-sp <spushparaj8807@gmail.com>
2025-09-05 16:27:37 +0530

    update css and config

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { HashRouter as Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { NotFound } from "./pages/NotFound";
 import { Toaster } from "@/components/ui/toaster";
